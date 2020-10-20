61be010dc07c62c805b8065febeed60da822d86a
Džiugas Eiva <dziugaseiva@gmail.com>
2020-10-20 21:42:54 +0100

    Complete 2.0.0

--- infiboots/config.cpp
+++ infiboots/config.cpp
@@ -1,7 +1,7 @@
-/*	
-	Copyright(C) 2019-2020 SteelShot <https://github.com/ModsBySteelShot>
-	
-	This program is free software: you can redistribute it and/or modify
+/*
+    Copyright(C) 2019-2020 SteelShot <https://github.com/ModsBySteelShot>
+
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -17,45 +17,45 @@
 
 class CfgPatches
 {
-	class InfiBoots
-	{
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = 1;
-		requiredAddons[] = {"DZ_Data"};
-	};
+    class InfiBoots
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1;
+        requiredAddons[] = {"DZ_Data", "DZ_Scripts"};
+    };
 };
 
 class CfgMods
 {
-	class InfiBoots
-	{
-		dir = "InfiBoots";
-		picture = "";
-		action = "";
-		hideName = 1;
-		hidePicture = 1;
-		name = "InfiBoots";
-		credits = "SteelShot";
-		author = "SteelShot";
-		authorID = "76561198029852322";
-		version = "1.0.0";
-		extra = 0;
-		type = "mod";
-		dependencies[] = {"Game"};
-		class defs
-		{
-			class gameScriptModule
-			{
-				value = "";
-				files[] = {"InfiBoots/scripts/3_Game"};
-			};
-			
-			class missionScriptModule
-			{
-				value = "";
-				files[] = {"InfiBoots/scripts/5_Mission"};
-			};
-		};
-	};
+    class InfiBoots
+    {
+        dir = "InfiBoots";
+        picture = "";
+        action = "";
+        hideName = 1;
+        hidePicture = 1;
+        name = "InfiBoots";
+        credits = "SteelShot";
+        author = "SteelShot";
+        authorID = "76561198029852322";
+        version = "1.0.0";
+        extra = 0;
+        type = "mod";
+        dependencies[] = {"World", "Mission"};
+        class defs
+        {
+            class worldScriptModule
+            {
+                value = "";
+                files[] = {"InfiBoots/scripts/4_World"};
+            };
+
+            class missionScriptModule
+            {
+                value = "";
+                files[] = {"InfiBoots/scripts/5_Mission"};
+            };
+        };
+    };
 };
---
+++ infiboots/scripts/4_World/Classes/InfiBoots.c
@@ -0,0 +1,95 @@
+/*
+    Copyright(C) 2019-2020 SteelShot <https://github.com/ModsBySteelShot>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+class InfiBoots
+{
+	private static ref InfiBootsConf config;
+
+    static InfiBootsConf GetConf()
+    {
+        return config;
+    }
+
+    static void Init()
+    {
+        config = new InfiBootsConf();
+    }
+}
+
+class InfiBootsConf
+{
+	private const string CONFIG_NAME = "InfiBoots.cfg";
+
+	private const string CONFIG_PATH = "$profile:InfiBoots.cfg"
+
+	private const float SHOES_MOVEMENT_DAMAGE_PER_STEP = 0.035;
+
+	private ref map<string, float> settings;
+
+	private void LoadConfig()
+    {
+        if (FileExist(CONFIG_PATH))
+        {
+            FileHandle f = OpenFile(CONFIG_PATH, FileMode.READ);
+
+            for (string line = ""; (FGets(f, line) != -1);)
+            {
+                TStringArray setting = new TStringArray;
+                line.Split("=", setting);
+
+                settings.Set(setting.Get(0), setting.Get(1).ToFloat());
+            }
+
+            CloseFile(f);
+
+            Print("[InfiBoots] " + CONFIG_NAME + " was found. All values have been loaded.");
+        }
+        else
+        {
+            CreateConfig();
+        }
+    }
+
+	private void CreateConfig()
+    {
+        FileHandle f = OpenFile(CONFIG_PATH, FileMode.WRITE);
+
+        foreach (string key : settings.GetKeyArray())
+        {
+            FPrintln(f, key + "=" + settings.Get(key));
+        }
+
+        CloseFile(f);
+
+        Print("[InfiBoots] " + CONFIG_NAME + " was not found. Default file created.");
+    }
+
+    void InfiBootsConf()
+    {
+        settings = new map<string, float>;
+
+        settings.Set("SHOES_MOVEMENT_DAMAGE_PER_STEP", SHOES_MOVEMENT_DAMAGE_PER_STEP);
+
+        if (GetGame().IsServer() && GetGame().IsMultiplayer())
+        {
+            LoadConfig();
+        }
+    }
+
+    float GetSettingValueByKey(string key)
+    {
+		return settings.Get(key);
+    }
+}
